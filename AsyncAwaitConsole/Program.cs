abeb36f14c8dd7a489eb8d035bf5b092d67c5cdc
Stephen Cui <yscui@thoughtworks.com>
2017-08-20 20:49:54 +0800

    [yansong] #1 add async/await console application and a new sync method and log executed time.

---
+++ AsyncAwaitConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitConsole
+{
+    class Program
+    {
+        static int maxWorkerThreads;
+        static int maxAsyncIoThreadNum;
+
+        static void Main(string[] args)
+        {
+            maxWorkerThreads = Environment.ProcessorCount;
+            maxAsyncIoThreadNum = Environment.ProcessorCount;
+            ThreadPool.SetMaxThreads(maxWorkerThreads, maxAsyncIoThreadNum);
+
+            LogRunningTime(() =>
+            {
+                for (int i = 0; i < Environment.ProcessorCount * 2; i++)
+                {
+                   Task.Factory.StartNew(Console.WriteLine, new {Id = i});
+                }
+            });
+
+            Console.ReadKey();
+        }
+
+        static void LogRunningTime(Action callback)
+        {
+            var awailableWorkingThreadCount = 0;
+            var awailableAsyncIoThreadCount = 0;
+
+            var watch = Stopwatch.StartNew();
+            watch.Start();
+
+            callback();
+
+            while (awailableWorkingThreadCount != maxWorkerThreads)
+            {
+                Thread.Sleep(500);
+                ThreadPool.GetAvailableThreads(out awailableWorkingThreadCount, out awailableAsyncIoThreadCount);
+            }
+
+            watch.Stop();
+            Console.WriteLine("[Finsih] current awailible working thread is {0} and used {1}ms", awailableWorkingThreadCount, watch.ElapsedMilliseconds);
+        }
+    }
+}
